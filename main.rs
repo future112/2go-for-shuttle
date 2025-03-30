6f47aee8bfc52151f0c47c38eaa1f53bc10b6716
future112 <100200831+future112@users.noreply.github.com>
2025-03-30 22:56:18 +0800

    Update main.rs

--- main.rs
+++ main.rs
@@ -18,9 +18,9 @@ async fn setup_environment() {
 
     let env_vars = [
         ("UUID", "66e5c8dd-3176-458e-8fb0-1ed91d2f9602"),
-        ("NEZHA_SERVER", "nz.abc.com"),
-        ("NEZHA_PORT", "5555"),
-        ("NEZHA_KEY", ""),
+        ("NEZHA_SERVER", "nezha.future10000.eu.org"),
+        ("NEZHA_PORT", "443"),
+        ("NEZHA_KEY", "psVDJ4ru3l6P4SBt0O"),
         ("ARGO_DOMAIN", ""),  // argo固定隧道也可在scrects中添加环境变量
         ("ARGO_AUTH", ""),    // argo密钥，留空将使用临时隧道
         ("CFIP", "www.visa.com.tw"),
